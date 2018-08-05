80e4a8a3b4ae06f79e32adad63f730151e98ff35
Wing leung CHOI <wingleungchoi@gmail.com>
2018-08-05 22:12:24 +0800

    setupServerLess

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # interview-transactions-wing-leung
+- node version: v8.10
+- deploy script:
+    - `$ AWS_ACCESS_KEY_ID=xxxxx AWS_SECRET_ACCESS_KEY=xxxxx  serverless deploy`
+    - note: serverless requires this AWS_ACCESS_KEY_ID has AdministratorAccess right in IAM
---
+++ handler.js
@@ -0,0 +1,15 @@
+module.exports.listTransactions = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify([
+      {
+        id: 1
+      },
+      {
+        id: 2
+      }
+    ]),
+  };
+
+  callback(null, response);
+};
